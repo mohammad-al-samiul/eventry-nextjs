e8df5cdfac3a4b28c5807bf8fbdd4fd721f8208b
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-07-26 11:10:03 +0600

    event list and event card added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import EventCard from "@/components/landing/EventCard";
 import Header from "@/components/landing/Header";
 
 export default function Home() {
   return (
-    <>
+    <section className="container">
       <Header />
-    </>
+      <EventCard />
+    </section>
   );
 }
---
+++ components/landing/EventCard.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import ActionButton from "../ActionButtons";
+
+export default function EventCard() {
+  return (
+    <div className="w-96 overflow-hidden rounded-md bg-[#242526]">
+      <Image
+        src="/assets/events/google-io-2023-1.png"
+        alt="Event 1"
+        width={500}
+        height={500}
+      />
+
+      <div className="p-3">
+        <a href="./details.html" className="font-bold text-lg">
+          Google I/O Extended
+        </a>
+        <p className="text-[#9C9C9C] text-sm mt-1">
+          Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh
+        </p>
+        <div className="text-[#737373] text-sm mt-1">
+          <span>1k Interested</span>
+          <span>|</span>
+          <span>40K Going</span>
+        </div>
+
+        <ActionButton />
+      </div>
+    </div>
+  );
+}
